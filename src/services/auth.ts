4199f799f150685d1cfe8bb72b9d9501bbfa08b8
adnaneal <adnane99lahkim@gmail.com>
2026-02-08 17:13:30 +0100

    connection au compte

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -29,3 +29,31 @@ export const Signin = async (email: string, password: string) => {
         }
     }
 };
+
+export const Signup = async (fullName: string, email: string, password: string) => {
+    try {
+        const response = await axios.post(`${API_URL}/auth/register`, {
+            fullName,
+            email,
+            password,
+        });
+
+        if (response.data.success) {
+            Cookies.set('token', response.data.accessToken, { expires: 15 });
+            return response.status;
+        } else {
+            toast.error(response.data.message)
+        }
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            if (error.response?.status === 401) {
+                toast.error(
+                    error.response.data?.message
+                );
+                return;
+            }
+
+            toast.error("Server Error !");
+        }
+    }
+};
