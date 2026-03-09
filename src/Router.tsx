3027a8ac17487c7f1fd2fb19d295802d44056a00
renda <renda.kouachi@gmail.com>
2026-03-09 14:26:52 +0100

    fix routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/home';
-import NotFoundPage from './pages/not-found';
-import LoginPage from './pages/Login/login';
-import RegisterPage from './pages/Login/sign_up';
-import PrivateRoute from './components/PrivateRoute';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home";
+import NotFoundPage from "./pages/not-found";
+import LoginPage from "./pages/Login/login";
+import RegisterPage from "./pages/Login/sign_up";
+import Dashboard from "./pages/Dashboard/home";
+import PrivateRoute from "./components/PrivateRoute";
 
 const AppRouter: React.FC = () => {
   return (
@@ -13,8 +13,9 @@ const AppRouter: React.FC = () => {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<PrivateRoute />}>
-        
+        <Route path="/" element={<PrivateRoute />}>
+          <Route index element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />} />
         </Route>
         {/* this one is used if url doesn't exist */}
         <Route path="*" element={<NotFoundPage />} />
